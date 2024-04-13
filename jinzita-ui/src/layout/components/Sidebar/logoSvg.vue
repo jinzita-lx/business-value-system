a5abd2fa0ca791b9faa72b5bab9406ed7d501e4b
liangxin <liangxin.710@bytedance.com>
2024-04-13 18:20:04 +0800

    feat: 短视频商业价值预测系统

---
+++ jinzita-ui/src/layout/components/Sidebar/logoSvg.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="-10 -10 50 50">
+    <path d="M 0 10 L 0 20 L 10 20 L 10 0 L 20 0 L 20 10 L 30 10 L 30 20 L 20 20 L 20 30 L 10 30 L 10 10" fill="#71f3eecc"/>
+    <path d="M 1 11 L 1 21 L 11 21 L 11 1 L 21 1 L 21 11 L 31 11 L 31 21 L 21 21 L 21 31 L 11 31 L 11 11" fill="#ea3354cc"/>
+    <path d="M 1 11 L 1 20 L 11 20 L 11 1 L 20 1 L 20 11 L 30 11 L 30 20 L 20 20 L 20 30 L 11 30 L 11 11" fill="#000000"/>
+  </svg>
+</template>
+<script>
+export default {
+}
+</script>
