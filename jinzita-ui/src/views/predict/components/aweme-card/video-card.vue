e88ce1ffd819f4da926bce83bd8ec18a34a52ad3
liangxin <liangxin.710@bytedance.com>
2024-05-09 12:42:42 +0800

    feat: 新增预测系统和大瓶完善

---
+++ jinzita-ui/src/views/predict/components/aweme-card/video-card.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+name: "video-card"
+}
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ jinzita-ui/src/views/predict/login/index.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+name: "index"
+}
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped lang="scss">
+
+</style>
