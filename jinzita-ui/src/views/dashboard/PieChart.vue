7a437e270272e2b3f59fa6202fc0bc9146505583
liangxin <liangxin.710@bytedance.com>
2024-05-22 01:31:19 +0800

    feat: 完成饼状图绘制

--- jinzita-ui/src/views/dashboard/PieChart.vue
+++ jinzita-ui/src/views/dashboard/PieChart.vue
@@ -1,87 +1,89 @@
 <template>
-  <div :class="className" :style="{height:height,width:width}" />
+  <div :class="className" :style="{ height: height, width: width }" />
 </template>
 
 <script>
-import * as echarts from 'echarts'
-require('echarts/theme/macarons') // echarts theme
-import resize from './mixins/resize'
-import { GetPieChart } from '@/api/resource/home-data'
-// 商业价值占比
+import * as echarts from "echarts";
+require("echarts/theme/macarons"); // echarts theme
+import resize from "./mixins/resize";
+import { GetPieChart } from "@/api/resource/home-data";
 export default {
   mixins: [resize],
   props: {
     className: {
       type: String,
-      default: 'chart'
+      default: "chart",
     },
     width: {
       type: String,
-      default: '100%'
+      default: "100%",
     },
     height: {
       type: String,
-      default: '100%'
-    }
+      default: "100%",
+    },
   },
   data() {
     return {
       chart: null,
       seriesData: [],
-    }
+    };
   },
   created() {
     this.initData();
   },
   beforeDestroy() {
     if (!this.chart) {
-      return
+      return;
     }
-    this.chart.dispose()
-    this.chart = null
+    this.chart.dispose();
+    this.chart = null;
   },
   methods: {
     async initData() {
       const res = await GetPieChart();
-      if(res.code === 200) {
-        console.log(res.data)
+      if (res.code === 200) {
         res.data.map((item, index) => {
           this.seriesData[index] = {
             value: item.typeNum,
-            name: item.typeName
-          }
-        })
-        this.seriesData.sort((a, b) => a.value - b.value)
+            name: item.typeName,
+          };
+        });
+        this.seriesData.sort((a, b) => a.value - b.value);
       }
       this.initChart();
     },
     initChart() {
-      this.chart = echarts.init(this.$el, 'macarons')
+      this.chart = echarts.init(this.$el, "macarons");
 
       this.chart.setOption({
         tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b} : {c} ({d}%)'
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)",
         },
-        // legend: {
-        //   left: 'center',
-        //   bottom: '10',
-        //   data: this.seriesData.map(item => item.name)
-        // },
+        legend: {
+          left: "center",
+          type: "scroll",
+          bottom: "10",
+          data: this.seriesData.map((item) => item.name),
+        },
         series: [
           {
-            name: '商业价值占比',
-            type: 'pie',
-            roseType: 'radius',
+            name: "商业价值占比",
+            type: "pie",
+            roseType: "area",
             radius: [15, 95],
-            center: ['50%', '38%'],
+            itemStyle: {
+              borderRadius: 10,
+            },
+            center: ["50%", "40%"],
+            animationEasing: "cubicInOut",
+            animationDuration: 2600,
             data: this.seriesData,
-            animationEasing: 'cubicInOut',
-            animationDuration: 2600
-          }
-        ]
-      })
-    }
-  }
-}
+          },
+        ],
+      });
+    },
+  },
+};
 </script>
